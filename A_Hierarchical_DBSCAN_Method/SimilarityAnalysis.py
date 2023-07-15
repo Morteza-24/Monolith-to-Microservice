dee38fad8c8271469b8ddfb9fa3e25ecd1a88205
morteza24 <mortezia4444.mz@gmail.com>
2023-07-15 09:45:09 +0330

    feat: SimilarityAnalysis.py sample

---
+++ A_Hierarchical_DBSCAN_Method/SimilarityAnalysis.py
@@ -0,0 +1,8 @@
+'''
+run java ./JavaParser/Parser
+open classes.json file
+
+def structrual_similarity() -> structural_similarity_matrix
+def semantic_similarity() -> semantic_similarity_matrix
+def class_similarity(structural_similarity_matrix, semantic_similarity_matrix) -> class_similarity_matrix
+'''
