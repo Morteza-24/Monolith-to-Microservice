cf684cfb386afac4ba4bcd6fc04a4aeb1ea3e996
morteza24 <mortezia4444.mz@gmail.com>
2023-07-15 07:18:51 +0330

    feat: Parser.java sample file

---
+++ A_Hierarchical_DBSCAN_Method/JavaParser/Parser.java
@@ -0,0 +1,14 @@
+/*
+
+import com.github.javaparser...
+CompilationUnit cu = StaticJavaParser.parse(new File(args[0]));
+cu.findAll(ClassOrInterfaceDeclaration.class).forEach(clazz -> {
+    Find all methods
+    Find all method calls
+    Get class source code
+}
+
+save results to classes.json
+
+*/
+
