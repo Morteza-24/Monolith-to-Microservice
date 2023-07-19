d8cd0268f3a92cb52fb495d1c0d874833393c68a
morteza24 <mortezia4444.mz@gmail.com>
2023-07-19 21:09:49 +0330

    fix: typo

--- A_Hierarchical_DBSCAN_Method/SimilarityAnalysis.py
+++ A_Hierarchical_DBSCAN_Method/SimilarityAnalysis.py
@@ -1,5 +1,5 @@
 '''
-def structrual_similarity(classes_info) -> structural_similarity_matrix
+def structural_similarity(classes_info) -> structural_similarity_matrix
 def semantic_similarity(classes_info) -> semantic_similarity_matrix
 def class_similarity(alpha, structural_similarity_matrix, semantic_similarity_matrix) -> class_similarity_matrix
 '''
