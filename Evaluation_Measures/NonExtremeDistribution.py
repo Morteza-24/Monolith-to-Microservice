40b60bae9086c428a725ee85ee40c7fa3260a250
paniztgp2002 <82807710+paniztgp2002@users.noreply.github.com>
2023-08-03 19:37:10 +0330

    feat: NonExtremeDistribution
    k and mi

--- Evaluation_Measures/NonExtremeDistribution.py
+++ Evaluation_Measures/NonExtremeDistribution.py
@@ -1,3 +1,16 @@
-def NED(microservices):
-    pass
+
+
+def count_unique_numbers_k(microservices):
+    unique_numbers_k = set(microservices)
+    k = len(unique_numbers_k)
+    return k
+
+
+def count_occurrences(microservices,k):
+    mi_values = []
+    for i in range(1, k + 1):
+        count =microservices.count(i)
+        mi_values.append(count)
+    
+    return mi_values
 
