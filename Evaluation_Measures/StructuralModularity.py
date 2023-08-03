d5ad5f56cc6982974f7ae2b2b95aa14e0d8d9c79
Amir-SaberHabibi <amirsaberhabibi80@gmail.com>
2023-08-04 01:12:05 +0330

    feat: SM init

--- Evaluation_Measures/StructuralModularity.py
+++ Evaluation_Measures/StructuralModularity.py
@@ -1,3 +1,24 @@
 def SM(microservices, classes_info):
-    pass
 
+    K = len(set(microservices)) 
+    SM = 0 
+    mu = [0] * K 
+    m = [0] * K 
+    sigma = [[0] * K for _ in range(K)]
+
+
+    # loop through the classes and count the calls (TBD)
+
+
+    # calculate the SM according to the formula
+    
+    for i in range(K):
+        SM += mu[i] / (m[i] ** 2) 
+    SM /= K
+    for i in range(K):
+        for j in range(K):
+            if i != j:
+                SM -= sigma[i][j] / (2 * m[i] * m[j])
+    SM /= (K * (K - 1)) / 2 
+
+    return SM 
