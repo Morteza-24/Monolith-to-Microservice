763dd739ec0a2a0fd30913d7c94e520db78cd3f7
paniztgp2002 <82807710+paniztgp2002@users.noreply.github.com>
2023-08-05 18:01:18 +0330

    update NonExtremeDistribution

--- Evaluation_Measures/NonExtremeDistribution.py
+++ Evaluation_Measures/NonExtremeDistribution.py
@@ -14,3 +14,26 @@ def count_occurrences(microservices,k):
     
     return mi_values
 
+def calculate_NED(k, mi_list):
+
+    count = 0
+
+    sum_mi = 0
+
+    for i in range(1, k+1):
+
+        mi = mi_list[i-1]
+
+        if 5 < abs(mi) < 20:
+
+            count += 1
+
+            sum_mi += mi
+
+    
+
+    ned = 1 - abs(sum_mi) / k
+
+    
+
+    return ned
