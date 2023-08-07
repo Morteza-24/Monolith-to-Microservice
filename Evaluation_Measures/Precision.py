8f164ffbeb157091a4fc5e6d2b3e26a0c1f215f2
morteza24 <mortezia4444.mz@gmail.com>
2023-08-07 19:04:49 +0330

    fix: Precision refactor

--- Evaluation_Measures/Precision.py
+++ Evaluation_Measures/Precision.py
@@ -1,16 +1,16 @@
-def corresponding(microservice, true_microservices):
+def _corresponding(microservice_classes, true_microservices):
     max_common_classes = 0
-    for i in set(true_microservices):
-        true_microservice = {clss for clss, ms in enumerate(true_microservices) if ms == i}
-        if (m := len(true_microservice.intersection(microservice))) > max_common_classes:
+    for true_ms in set(true_microservices):
+        true_ms_classes = {clss for clss, ms in enumerate(true_microservices) if ms == true_ms}
+        if (m := len(true_ms_classes.intersection(microservice_classes))) > max_common_classes:
             max_common_classes = m
-            true_correspondence = true_microservice
-    return true_correspondence
+            corresponding_ms = true_ms_classes
+    return corresponding_ms
 
 
 def precision(microservices, true_microservices):
     summation = 0
-    for i in set(microservices):
-        microservice = {clss for clss, ms in enumerate(microservices) if ms == i}
-        summation += len(microservice.intersection(corresponding(microservice, true_microservices))) / len(microservice)
+    for microservice in set(microservices):
+        ms_classes = {clss for clss, ms in enumerate(microservices) if ms == microservice}
+        summation += len(microservice.intersection(_corresponding(ms_classes, true_microservices))) / len(ms_classes)
     return summation / len(set(microservices))
