d1d9a654bd69780184f007a564ff772ebf5f72dc
morteza24 <mortezia4444.mz@gmail.com>
2023-11-30 18:50:23 +0330

    fix: CoGCN experiments

--- CoGCN_EvaluationMeasures.py
+++ CoGCN_EvaluationMeasures.py
@@ -4,9 +4,10 @@ from json import load
 from EvaluationMeasures import *
 
 
-proj = "JPetStore"
-microservices = [0,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,2,3,0,0,4,0,1,2,2,0,0,5,0,0,0,0]
+proj = ""
+microservices = []
 classes_order = {}
+
 project_directory = None
 
 
