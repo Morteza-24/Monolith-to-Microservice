41a802d81d55c7a8c4a6f3006fa206f20d36f3aa
Morteza-24 <mortezia4444.mz@gmail.com>
2025-01-01 07:52:49 +0330

    completed Mo2oM_expt

--- Mo2oM_expt.py
+++ Mo2oM_expt.py
@@ -10,7 +10,7 @@ import numpy as np
 parser = ArgumentParser(
     prog='python Mo2oM_expt.py',
     description='Experiment versoin of the Mo2oM tool.',
-    epilog='example usage: python Mo2oM_expt.py -f Test_Projects/JPetStore/OneFileSource.java --alpha 1 --n-clusters 3 --threshold 0.1 0.7 --n-execs 10 -e ICP NED -o output.json')
+    epilog='example usage: python Mo2oM_expt.py -f Test_Projects/JPetStore/OneFileSource.java --n-clusters 3 --threshold 0.1 0.7 -e ICP NED -o output.json')
 
 parser.add_argument("-f", "--file", dest="file_path",
                     help="path to the java source code file (use this option if your whole monolithic program is in one file)")
@@ -24,7 +24,7 @@ parser.add_argument("-e", "--evaluation-measure", choices=["Precision", "SR", "S
 parser.add_argument("-k", type=int, nargs="*",
                     help="The k value for the SR measure (e.g. SR@7). This option can only be used if you are using the SR measure.")
 parser.add_argument("--n-clusters", dest="n_clusters", type=int, nargs="*", default=[None],
-                    help="number of clusters hyperparameter, Enter two values to run the method on an interval of n_clusters values. Leave empty to use Scanniello's approach")
+                    help="number of clusters hyperparameter, Enter two values to run the method on an interval of n_clusters values. Leave empty to use Scanniello's approach.")
 parser.add_argument("--threshold", dest="threshold", type=float, nargs="*", default=[None],
                     help="degree of membership threshold hyperparameter. Enter two values to run the method on an interval of threshold values.")
 
@@ -54,6 +54,10 @@ if args.evaluation_measure:
 measures = {"Precision": Precision, "SR": SR,
             "SM": SM, "IFN": IFN, "NED": NED, "ICP": ICP}
 base_dir = path.dirname(path.realpath(__file__))
+
+
+def _listify(x):
+    return x if isinstance(x, list) else [x]
 
 
 def merge_java_files(src_dir, dest_file):
@@ -96,10 +100,15 @@ print("\n--- Mo2oM ---")
 outputs = []
 
 if len(args.n_clusters) == 2:
-    n_clusters = np.arange(args.n_clusters[0], args.n_clusters[1]+1, 1)
-else:
+    n_clusters = list(np.arange(args.n_clusters[0], args.n_clusters[1]+1, 1))
+elif args.n_clusters == [None]:
     n_clusters = "Scanniello"
-thresholds = [round(_, 3) for _ in np.arange(args.threshold[0], args.threshold[1]+0.01, 0.05)]
+else:
+    n_clusters = args.n_clusters[0]
+if len(args.threshold) == 2:
+    thresholds = [round(_, 3) for _ in np.arange(args.threshold[0], args.threshold[1]+0.01, 0.05)]
+else:
+    thresholds = args.threshold[0]
 clusters, classes_info = Mo2oM(args.file_path, n_clusters, thresholds)
 class_names = list(classes_info)
 if args.project_directory:
@@ -109,26 +118,32 @@ if args.project_directory:
             true_microservices[class_names.index(clss)].add(i)
             if -1 in true_microservices[class_names.index(clss)]:
                 true_microservices[class_names.index(clss)].discard(-1)
-for i in range(len(clusters)):
-    for j in range(len(thresholds)):
-        if isinstance(n_clusters, str) and n_clusters == "Scanniello":
-            n_clusters_i = i + 2
+if n_clusters == "Scanniello":
+    n_clusters = list(np.arange(2, (len(classes_info)//2)+2, 2))
+for i in range(len(_listify(n_clusters))):
+    for j in range(len(_listify(thresholds))):
+        if isinstance(n_clusters, int) and isinstance(thresholds, int):
+            this_clusters = clusters
+        elif isinstance(n_clusters, int):
+            this_clusters = clusters[j]
+        elif isinstance(thresholds, int):
+            this_clusters = clusters[i]
         else:
-            n_clusters_i = int(n_clusters[i])
-        output = {"n_clusters": n_clusters_i,
-                    "threshold": float(thresholds[j]),
-                    "microservices": [[int(_i) for _i in _] for _ in clusters[i][j]]}
+            this_clusters = clusters[i][j]
+        output = {"n_clusters": int(_listify(n_clusters)[i]),
+                  "threshold": float(_listify(thresholds)[j]),
+                  "microservices": [[int(_i) for _i in _] for _ in this_clusters]}
         if args.evaluation_measure:
             for measure in args.evaluation_measure:
                 if measure in ["SM", "IFN", "ICP"]:
-                    output[measure] = measures[measure](clusters[i][j], classes_info)
+                    output[measure] = measures[measure](output["microservices"], classes_info)
                 elif measure == "Precision":
-                    output[measure] = measures[measure](clusters[i][j], true_microservices)
+                    output[measure] = measures[measure](output["microservices"], true_microservices)
                 elif measure == "SR":
                     for k in args.k:
-                        output[measure+"@"+str(k)] = measures[measure](clusters[i][j], true_microservices, k)
+                        output[measure+"@"+str(k)] = measures[measure](output["microservices"], true_microservices, k)
                 else:
-                    output[measure] = measures[measure](clusters[i][j])
+                    output[measure] = measures[measure](output["microservices"])
         outputs.append(output)
 
 with open("tmp_"+args.output_file, "w") as output_file:
